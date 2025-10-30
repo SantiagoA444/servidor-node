531a8c35bc0bfc6c2c8c3d30b33fc3325ff0cde1
SantiagoA444 <salfarov048@inemkennedy.edu.co>
2025-10-30 16:44:41 -0500

    Agregado servidor Express con index.js y dependencias

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from "express"
+import cors from "cors";
+
+const app=express();
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('nuestro servidor funciona 😁');
+});
+
+app.listen(3000, () => {
+    console.log('Servidor Corriendo en http://localhost:3000');
+});
